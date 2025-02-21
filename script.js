85ee2322e9a29d3105e618aa92d23cdabc9cbc89
GuilhermeFranG <qhilherme@gmail.com>
2025-02-20 22:10:35 -0300

    Evento de scroll menu fixo.

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function showRecipe(index) {
     isTransitioning = true;
 
     recipes.forEach((recipe, i) => {
-        if (i === index) {
-            setTimeout(() => recipe.classList.add('active'), 350);
+        if (i === index) { // Se o valor i: número da receita, for igual a o index torna-a visível
+            setTimeout(() => recipe.classList.add('active'), 300);
             recipe.classList.remove('hidden');
-            setTimeout(() => isTransitioning = false, 500);
-        } else {
+            setTimeout(() => isTransitioning = false, 500); // timer para usar os botões novamente
+        } else { // Se não, display: none; 
             setTimeout(() => recipe.classList.add('hidden'), 300);
             recipe.classList.remove('active');
             setTimeout(() => isTransitioning = false, 500);
@@ -24,16 +24,16 @@ function showRecipe(index) {
     });
 }
 
-// Evento para o botão "Próximo"
+// Botão next
 nextBtn.addEventListener('click', () => {
-    if (!isTransitioning) {
+    if (!isTransitioning) { 
         currentIndex = (currentIndex + 1) % recipes.length;
         // % unidade binária: retorna o resto de uma divisão. Ex.: 12 % 5 = 2, 1 % 5 = 1 e 15 % 2 = 1  
         showRecipe(currentIndex);
     }
 });
 
-// Evento para o botão "Anterior"
+// Botão previous
 prevBtn.addEventListener('click', () => {
     if (!isTransitioning) {
         currentIndex = (currentIndex - 1 + recipes.length) % recipes.length;
@@ -43,3 +43,19 @@ prevBtn.addEventListener('click', () => {
 
 // Inicializa com a primeira receita visível
 showRecipe(currentIndex);
+
+
+// Evento de scroll no menu fixo
+let lastScroll = window.scrollY; // Armazena a posição inicial do scroll
+
+window.addEventListener("scroll", function () {
+    let currentScroll = window.scrollY; // Posição atual depois do último scroll
+    // .scrollY = pega o valor do scroll vertical da página
+    if (currentScroll < lastScroll) {
+        document.getElementById("menu-container").style.top = "0px";
+    } else {
+        document.getElementById("menu-container").style.top = "-50px";
+    }
+
+    lastScroll = currentScroll; // Atualiza a posição do scroll
+});
